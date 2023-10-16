77887672de51ff14cd84c08e31c47a81101f8eca
Кирилл Петухов <test@mail.ru>
2023-10-16 22:47:25 +0300

    env file

--- src/models/users.py
+++ src/models/users.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, DateTime, String
 from sqlalchemy.dialects.postgresql import UUID
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from db.postgres import Base
+from src.db.postgres import Base
 
 
 class User(Base):
