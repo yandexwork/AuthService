8561aedbaea74ede52d34a40315c650af13284f9
Danila Kalganov <dkalganov@team.amocrm.com>
2023-11-05 17:46:30 +0300

    Исправил redis соеденение

--- src/db/redis_db.py
+++ src/db/redis_db.py
@@ -1,11 +1,9 @@
-from typing import Optional
 from redis.asyncio import Redis
 
 from src.core.config import settings
 
 
-redis: Optional[Redis] = None
-redis.redis = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
+redis = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 
 async def get_redis() -> Redis:
